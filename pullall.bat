4237cae7be72f451f72280a6b1260df0869de9fd
lifefountain855 <kevgumball@gmail.com>
2026-08-03 19:30:41 -0400

    init1

--- pullall.bat
+++ pullall.bat
@@ -1,2 +1,4 @@
-git subtree pull --prefix=BP/ bp_repo main --squash
-git subtree pull --prefix=RP/ rp_repo main --squash
+git subtree add --prefix=BP/ bp_repo main --squash
+git subtree add --prefix=RP/ rp_repo main --squash
+@REM git subtree pull --prefix=BP/ bp_repo main --squash
+@REM git subtree pull --prefix=RP/ rp_repo main --squash
