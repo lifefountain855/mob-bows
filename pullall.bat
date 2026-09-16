e9c9b317041e43696250c9d4b3e33a70a5970743
lifefountain855 <kevgumball@gmail.com>
2026-09-16 17:09:31 -0400

    pullall change

--- pullall.bat
+++ pullall.bat
@@ -1,4 +1,4 @@
 @REM git subtree add --prefix=BP/ bp_repo main --squash
 @REM git subtree add --prefix=RP/ rp_repo main --squash
-git subtree pull --prefix=BP/ bp_repo main --squash --no-edit
-git subtree pull --prefix=RP/ rp_repo main --squash --no-edit
+git subtree pull --prefix=BP/ bp_repo main --squash
+git subtree pull --prefix=RP/ rp_repo main --squash
